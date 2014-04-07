36983da892f8f45780c1b1f55db27642f1063d53
Junaos <Junaos@junaos.com>
2014-04-07 12:10:43 -0700

    Switched local env to postgres

--- review/review/settings/local.py
+++ review/review/settings/local.py
@@ -26,10 +26,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': normpath(join(DJANGO_ROOT, 'default.db')),
-        'USER': '',
-        'PASSWORD': '',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'reviews',
+        'USER': 'review',
+        'PASSWORD': 'gLD]kCT95@/6afGu',
         'HOST': '',
         'PORT': '',
     }
